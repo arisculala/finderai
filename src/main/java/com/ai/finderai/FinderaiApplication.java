b0705440b444431ab7ff3b6189b14e77a3e66528
arisculala <arisculala@gmail.com>
2025-03-21 20:30:57 +0800

    feature: added liquibase database versioning

--- src/main/java/com/ai/finderai/FinderaiApplication.java
+++ src/main/java/com/ai/finderai/FinderaiApplication.java
@@ -2,13 +2,14 @@ package com.ai.finderai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @OpenAPIDefinition(info = @Info(title = "FinderAI Service API", version = "0.0.1", description = "API for FinderAI Service"))
 @SpringBootApplication(scanBasePackages = { "com.ai", "com.generic" })
-public class FinderaiApplication {
+public class FinderaiApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(FinderaiApplication.class, args);
     }
